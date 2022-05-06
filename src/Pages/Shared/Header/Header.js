52e56b172d9a893af51569f3d96f25c7c6f7a101
Hasanat902 <abulhasanat902@gmail.com>
2022-05-06 10:57:47 +0600

    navbar added

---
+++ src/Pages/Shared/Header/Header.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import './Header.css';
+
+const Header = () => {
+  return (
+    <div>
+      <Navbar collapseOnSelect expand="lg" bg="" className="background" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home" className="fw-bold fs-3">CAR <span className="text-success">BD</span></Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#features" className="text-dark fs-6">Home</Nav.Link>
+              
+            </Nav>
+            <Nav>
+              <Nav.Link href="#deets" className="text-dark fs-6">Blogs</Nav.Link>
+              
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
+  );
+};
+
+export default Header;
