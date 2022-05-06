78984abfe80c123fee80f33498a0b78a9218a00c
Hasanat902 <abulhasanat902@gmail.com>
2022-05-06 21:44:42 +0600

    not found page added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Blogs from './Pages/Blogs/Blogs';
 import Home from './Pages/Home/Home/Home';
 import Footer from './Pages/Shared/Footer/Footer';
 import Header from './Pages/Shared/Header/Header';
+import NotFound from './Pages/Shared/NotFound/NotFound';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path='/' element={<Home></Home>}></Route>
         <Route path='/home' element={<Home></Home>}></Route>
         <Route path='/blogs' element={<Blogs></Blogs>}></Route>
+        <Route path='*' element={<NotFound></NotFound>}></Route>
       </Routes>
       <Footer></Footer>
     </div>
--- src/Pages/Shared/NotFound/NotFound.js
+++ src/Pages/Shared/NotFound/NotFound.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import notfound from '../../../images/not-found.jpg';
 
 const NotFound = () => {
     return (
-        <div>
-            
+        <div className='text-center mt-5' style={{height: "79vh"}}>
+            <h2 className='mb-4 text-danger'>The page you requested is not found</h2>
+            <img className='img-fluid' src={notfound} alt="" />
         </div>
     );
 };
