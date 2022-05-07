44f10cc184e1624cb60f2721c12007d254e1e9f9
Hasanat902 <abulhasanat902@gmail.com>
2022-05-07 11:49:57 +0600

    implement signout and password reset

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { Button, Form } from 'react-bootstrap';
-import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { useSendPasswordResetEmail, useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import { Link, useNavigate } from 'react-router-dom';
 import auth from '../../../firebase.init';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Login = () => {
 
+    const emailRef = useRef('');
+    const passwordRef = useRef('');
     const navigate = useNavigate();
+    let errorMessage;
 
     const [
         signInWithEmailAndPassword,
@@ -15,6 +20,12 @@ const Login = () => {
         error,
       ] = useSignInWithEmailAndPassword(auth);
 
+    const [sendPasswordResetEmail] = useSendPasswordResetEmail(auth);
+
+    if(error){
+        errorMessage = <p className='text-danger'>Error: {error?.message}</p>;
+    }
+
     if(user){
         navigate('/home');
     }
@@ -22,11 +33,21 @@ const Login = () => {
     const handleSubmit = event => {
         event.preventDefault();
 
-        const email = event.target.email.value;
-        const password = event.target.password.value;
+        const email = emailRef.current.value;
+        const password = passwordRef.current.value;
 
-        signInWithEmailAndPassword(email, password);
-        
+        signInWithEmailAndPassword(email, password);        
+    }
+
+    const resetPassword = async() => {
+        const email = emailRef.current.value;
+        if(email){
+            await sendPasswordResetEmail(email);
+            toast("Sent Email");
+        }
+        else{
+            toast("Please enter your email address first");
+        }
     }
 
     return (
@@ -35,18 +56,21 @@ const Login = () => {
             <Form onSubmit={handleSubmit}>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" name="email" placeholder="Enter email" required />
+                    <Form.Control type="email" ref={emailRef} placeholder="Enter email" required />
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" name="password" placeholder="Password" required />
+                    <Form.Control type="password" ref={passwordRef} placeholder="Password" required />
                 </Form.Group>
+                {errorMessage}
                 <Button className='w-50 mx-auto d-block' variant="info" type="submit">
                     Login
                 </Button>
             </Form>
-            <p className='mt-2'>Don't have an account? <Link to='/register' className='text-decoration-none'>Please Register</Link></p>
+            <p className='mt-2'>Forgot Password?<button onClick={resetPassword} className='btn btn-link text-decoration-none'>Reset Password</button></p>
+            <p>Don't have an account? <Link to='/register' className='text-decoration-none'>Please Register</Link></p>
+            <ToastContainer></ToastContainer>
         </div>
     );
 };
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,9 +1,19 @@
+import { signOut } from "firebase/auth";
 import React from "react";
 import { Container, Nav, Navbar } from "react-bootstrap";
+import { useAuthState } from "react-firebase-hooks/auth";
 import { Link } from "react-router-dom";
+import auth from "../../../firebase.init";
 import './Header.css';
 
 const Header = () => {
+
+  const [user] = useAuthState(auth);
+
+  const handleSignOut = () => {
+    signOut(auth);
+  }
+
   return (
     <div>
       <Navbar collapseOnSelect expand="lg" bg="info" className="background" variant="dark">
@@ -18,7 +28,12 @@ const Header = () => {
               <Nav.Link as={Link} to="/home" className="text-dark fs-6 fw-bold">Home</Nav.Link>
               <Nav.Link as={Link} to="/blogs" className="text-dark fs-6 fw-bold">Blogs</Nav.Link>
 
-              <Nav.Link as={Link} to="/login" className="text-dark fs-6 fw-bold">LogIn</Nav.Link>
+              {
+                user ?
+                <button onClick={handleSignOut} className="btn btn-link text-decoration-none text-dark fs-6 fw-bold">SignOut</button>
+                : 
+                <Nav.Link as={Link} to="/login" className="text-dark fs-6 fw-bold">LogIn</Nav.Link>
+              }
               
             </Nav>
           </Navbar.Collapse>
